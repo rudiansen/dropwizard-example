4e4415f731ae022d2768c784e10ddd39f2b7c969
Rudiansen Gunawan <rudiansen.gunawan@id.abb.com>
2018-07-10 17:03:14 +0800

    add more dependencies

--- src/main/java/com/example/helloworld/HelloWorldConfiguration.java
+++ src/main/java/com/example/helloworld/HelloWorldConfiguration.java
@@ -1,10 +1,53 @@
 package com.example.helloworld;
 
-import io.dropwizard.Configuration;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.*;
-import javax.validation.constraints.*;
+
+import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
 
 public class HelloWorldConfiguration extends Configuration {
-    // TODO: implement service configuration
+	
+	@NotEmpty
+	private String template;
+	@NotEmpty
+	private String defaultName = "Stranger";
+
+	@Valid
+	@NotNull
+	private DataSourceFactory database = new DataSourceFactory();
+	
+	@JsonProperty
+	public String getTemplate() {
+		return template;
+	}
+
+	@JsonProperty
+	public void setTemplate(String template) {
+		this.template = template;
+	}
+
+	@JsonProperty
+	public String getDefaultName() {
+		return defaultName;
+	}
+
+	@JsonProperty
+	public void setDefaultName(String defaultName) {
+		this.defaultName = defaultName;
+	}
+	
+	@JsonProperty("database")
+	public DataSourceFactory getDataSourceFactory() {
+		return database;
+	}
+
+	@JsonProperty("database")
+	public void setDataSourceFactory(DataSourceFactory database) {
+		this.database = database;
+	}
 }
