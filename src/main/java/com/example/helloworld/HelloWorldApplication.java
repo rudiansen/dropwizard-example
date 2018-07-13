2dc8e6d8b5d5b9ef065abe2f3d39b423abf67283
Rudiansen Gunawan <rudiansen.gunawan@id.abb.com>
2018-07-13 11:54:05 +0800

    add authentication

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -2,15 +2,24 @@ package com.example.helloworld;
 
 import java.util.Map;
 
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
+
+import com.example.helloworld.auth.ExampleAuthenticator;
+import com.example.helloworld.auth.ExampleAuthorizer;
 import com.example.helloworld.core.Person;
 import com.example.helloworld.core.Template;
+import com.example.helloworld.core.User;
 import com.example.helloworld.db.PersonDAO;
 import com.example.helloworld.health.TemplateHealthCheck;
 import com.example.helloworld.resources.HelloWorldResource;
 import com.example.helloworld.resources.PeopleResource;
 import com.example.helloworld.resources.PersonResource;
+import com.example.helloworld.resources.ProtectedResource;
 
 import io.dropwizard.Application;
+import io.dropwizard.auth.AuthDynamicFeature;
+import io.dropwizard.auth.AuthValueFactoryProvider;
+import io.dropwizard.auth.basic.BasicCredentialAuthFilter;
 import io.dropwizard.db.PooledDataSourceFactory;
 import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.migrations.MigrationsBundle;
@@ -62,10 +71,18 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
     	final PersonDAO dao = new PersonDAO(hibernateBundle.getSessionFactory());
     	final Template template = configuration.buildTemplate();
     	
-    	environment.healthChecks().register("template", new TemplateHealthCheck(template));    	
+    	environment.healthChecks().register("template", new TemplateHealthCheck(template));
+    	environment.jersey().register(new AuthDynamicFeature(new BasicCredentialAuthFilter.Builder<User>()
+    			.setAuthenticator(new ExampleAuthenticator())
+    			.setAuthorizer(new ExampleAuthorizer())
+    			.setRealm("SUPER SECRET STUFF")
+    			.buildAuthFilter()));
+    	environment.jersey().register(new AuthValueFactoryProvider.Binder<>(User.class));
+    	environment.jersey().register(RolesAllowedDynamicFeature.class);
     	environment.jersey().register(new PeopleResource(dao));
     	environment.jersey().register(new PersonResource(dao));
     	environment.jersey().register(new HelloWorldResource(template));
+    	environment.jersey().register(new ProtectedResource());
     	
     }
 
---
+++ src/main/java/com/example/helloworld/core/User.java
@@ -0,0 +1,34 @@
+package com.example.helloworld.core;
+
+import java.security.Principal;
+import java.util.Set;
+
+public class User implements Principal {
+	
+	private final String name;
+	
+	private final Set<String> roles;
+	
+	public User(String name) {
+		this.name = name;
+		this.roles = null;
+	}
+
+	public User(String name, Set<String> roles) {
+		this.name = name;
+		this.roles = roles;
+	}
+
+	public int getId() {
+		return (int) (Math.random() * 100);
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public Set<String> getRoles() {
+		return roles;
+	}		
+
+}
